fbc04a24eeb41665f2ba77ba99e1ab318396a7bc
mattwylder <matt@wylder.us>
2015-10-14 16:12:29 -0500

    Refactored methods

--- curse.py
+++ curse.py
@@ -1,22 +1,24 @@
 import curses
 import praw
 
+#Global Vars probably bad design
+first = 2
+between = 2    
+limit = 10;
+r = praw.Reddit(user_agent='redditty')
+submissions_gen = r.get_subreddit('games').get_top(limit=limit)   
+stdscr = curses.initscr()
+subs = []
+
 def main():
-    first = 2
-    between = 2    
     curses.cbreak()
     curses.start_color()
     curses.noecho()
     stdscr.keypad(1)
     i = first
-    for submission in submissions:
-	title = submission.title
-	title = title[:80]
-	stdscr.addstr(i,4,title)
-	i= i+between
-    stdscr.refresh()
+    populate_screen()
     loc = first
-    chhilite(loc)
+    change_highlight(loc)
     while(True):
 	c = stdscr.getch()
 	if c == ord('j') and loc <= limit*between -2:
@@ -24,19 +26,25 @@ def main():
 	    chhilite(loc)
 	if c == ord('k') and loc >= first:
 	    loc=loc-between
-	    chhilite(loc)
+	    change_highlight(loc)
     #curses.endwin()
-   
-def chhilite(location):
+
+#Pass submissions in?
+def populate_screen():
+    i=first
+    for submission in submissions_gen:
+	title = submission.title
+	subs.append(submission)
+	title = title[:80]
+	stdscr.addstr(i,4,title)
+	i= i+between
+    stdscr.refresh()
+
+#Pass subs?   
+def change_highlight(location):
     #title = submissions[location]
-    stdscr.addstr(location, 4, 'here i am ', curses.A_UNDERLINE)
+    stdscr.addstr(location, 4, subs[location/2 - 1].title, curses.A_UNDERLINE)
     stdscr.refresh()
     curses.flash()
-
-limit = 10;
-r = praw.Reddit(user_agent='reddpyxx')
-submissions = r.get_subreddit('games').get_top(limit=limit)
-    
-stdscr = curses.initscr()
 
 main()
